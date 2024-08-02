fb78a954d71281e3ee0f479bc69e7e26a1b9609a
pratham1916 <120631770+pratham1916@users.noreply.github.com>
2024-08-02 13:21:28 +0530

    Modified Fronted Dashboard

--- Server/config/db.config.js
+++ Server/config/db.config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connection  = mongoose.connect("mongodb+srv://pratham11:pratham11@swanirbhar.ofvfqws.mongodb.net/Swanirbhar_LMS?retryWrites=true&w=majority&appName=Swanirbhar")
+const connection  = mongoose.connect(process.env.MONGO_URL)
 
 module.exports={
     connection
